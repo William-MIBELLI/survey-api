bae5541613ee575010b967acd09ab2cdb19d5d7c
William Mibelli <william.mibelli@gmail.com>
2025-10-05 02:13:25 +0200

    Reapply "switch from tsx to ts-node and nodemon, now watch works on docker"
    This reverts commit a120b751abb99da6eb36b866049287ff41ea3267.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,42 @@
-import resolvers from "#resolvers/index.js";
-import express from "express";
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@as-integrations/express5';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import resolvers from './resolvers/index';
+import typeDefs from './schemas/index'
+
+interface MyContext {
+  token?: string;
+}
 const app = express();
 
-const port = process.env.PORT || 27000;
+const httpServer = http.createServer(app);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-  console.log("Response sent after");
+const server = new ApolloServer<MyContext>({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port 🍆 zgeg ${port}`);
-});
+const main = async () => {
+
+  await server.start();
+  
+  app.use(
+    '/',
+    cors<cors.CorsRequest>(),
+    express.json(),
+  
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    }),
+  );
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: process.env.PORT }, resolve),
+  );
+  console.log(`🚀 Server ready at http://localhostzgeg:${process.env.PORT}/`)
+}
+
+main()
