519f6609eecaad42034f6b34455ab07a5e264259
William Mibelli <william.mibelli@gmail.com>
2025-11-14 12:03:56 +0100

    add method for check question is from user

--- src/services/question.service.ts
+++ src/services/question.service.ts
@@ -2,10 +2,22 @@ import QuestionEntity from "entities/question.entity";
 import GenericService from "./generic.service";
 import { Repository } from "typeorm";
 
-
 export default class QuestionService extends GenericService<QuestionEntity> {
-
   constructor(repo: Repository<QuestionEntity>) {
-    super(repo)
+    super(repo);
+  }
+
+  public async findByIDAndCheckOwner(
+    questionId: string,
+    userId: string,
+  ): Promise<QuestionEntity | null> {
+    return await this.repo.findOne({
+      where: {
+        id: questionId,
+        survey: {
+          ownerId: userId,
+        },
+      },
+    });
   }
 }
