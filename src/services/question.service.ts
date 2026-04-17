be228315e35e7f5cee222aa96546b9328d890a31
William Mibelli <william.mibelli@gmail.com>
2026-04-17 19:42:43 +0200

    WIP updateQuestion in controller

--- src/services/question.service.ts
+++ src/services/question.service.ts
@@ -2,10 +2,27 @@ import QuestionEntity from "entities/question.entity";
 import GenericService from "./generic.service";
 import { Repository } from "typeorm";
 import { GraphQLError } from "graphql";
+import { UpdateQuestionInput } from "generated/graphql";
 
 export default class QuestionService extends GenericService<QuestionEntity> {
   constructor(repo: Repository<QuestionEntity>) {
     super(repo);
+  }
+
+  public async updateQuestion(args: UpdateQuestionInput): Promise<QuestionEntity> {
+    const question = await this.findById(args.id);
+    if (!question) {
+      throw new GraphQLError("No question found.");
+    }
+    const { options, ...rest } = args;
+
+    //UPDATE DE LA QUESTION
+    const updatedQuestion = this.updateOne(question, rest);
+
+    //GESTION DES OPTIONS
+    if (args.options) {
+    }
+    return updatedQuestion;
   }
 
   public async checkQuestionIsFromUser(
@@ -21,8 +38,8 @@ export default class QuestionService extends GenericService<QuestionEntity> {
       },
     });
     if (!question) {
-      throw new GraphQLError("No question found.")
+      throw new GraphQLError("No question found.");
     }
-    return question
+    return question;
   }
 }
